5d256912660245d095448d4264e34d6a2945a965
Jeff Toppings <jeffrey.toppings@outlook.com>
2023-06-17 10:23:10 -0600

    feat: api call wired up properly

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,29 +1,47 @@
 import React from "react";
 import { useEffect } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { categoryMap } from "../utils/utils";
+
+interface Question {
+  question: string;
+  options: string[];
+  correctOption: string[];
+}
+
+interface GameInfo {
+  questions: Question[];
+  totalQuestions: number;
+  progress: {
+    correct: number;
+    questionsAsked: number;
+  };
+}
 
 export default function Game({ route, navigation }) {
   const [isLoading, setLoading] = React.useState(false);
   const [data, setData] = React.useState<any>(null);
 
   useEffect(() => {
-    console.log("use effect", route.params);
-    // TODO update this with actual params
-    const url = "https://opentdb.com/api.php?amount=10";
+    const amount = route?.params?.numQuestions;
+    const category = route?.params?.category;
+    const difficulty = route?.params?.difficulty;
+    const url = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`;
     fetch(url)
       .then((resp) => resp.json())
-      .then((json) => setData(json))
+      .then((json) => updateData(json?.results))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
 
+  function updateData(data: any): void {
+    console.log(data);
+    setData(data);
+  }
+
   return (
     <View style={styles.container}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <Text>Game works!</Text>
-      )}
+      {isLoading ? <ActivityIndicator /> : <Text>Game works!</Text>}
     </View>
   );
 }
