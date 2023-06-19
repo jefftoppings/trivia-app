49d5c983533b8ebf58911a687dd361fbf3f4e56c
Jeff Toppings <jeffrey.toppings@outlook.com>
2023-06-18 18:44:12 -0600

    feat: Finish game loop, needs some styling

--- components/Game.tsx
+++ components/Game.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useEffect } from "react";
 import {
   ActivityIndicator,
+  Button,
+  Image,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -27,11 +29,15 @@ interface GameInfo {
   totalQuestions: number;
 }
 
+type GameState = "question" | "answer" | "result";
+
 export default function Game({ route, navigation }) {
   const [isLoading, setLoading] = React.useState(false);
   const [gameInfo, setGameInfo] = React.useState<GameInfo>(null);
-  const [correct, setCorrect] = React.useState<number>(null);
+  const [numCorrect, setNumCorrect] = React.useState<number>(null);
   const [progressIndex, setProgressIndex] = React.useState<number>(null);
+  const [gameState, setGameState] = React.useState<GameState>("question");
+  const [questionCorrect, setQuestionCorrect] = React.useState<boolean>(false);
 
   useEffect(() => {
     const amount = route?.params?.numQuestions;
@@ -63,7 +69,7 @@ export default function Game({ route, navigation }) {
     };
     console.log({ gameInfo });
     setGameInfo(gameInfo);
-    setCorrect(0);
+    setNumCorrect(0);
     setProgressIndex(0);
   }
 
@@ -78,34 +84,114 @@ export default function Game({ route, navigation }) {
    * fixes some issues from API responses
    */
   function decodeHtmlCharCodesInString(val: string): string {
-    return val.replaceAll("&quot;", '"');
+    return val
+      .replaceAll("&quot;", '"')
+      .replaceAll("&#039;", "'")
+      .replaceAll("&rsquo;", "'")
+      .replaceAll("&amp;", "&");
   }
 
-  const actualGame = (
-    <View>
-      <Text
-        style={{
-          fontSize: 16,
-          paddingTop: 8,
-          paddingBottom: 8,
-        }}
-      >
-        {gameInfo?.totalQuestions
-          ? ["Question", progressIndex + 1, "of", gameInfo.totalQuestions].join(
-              " "
-            )
-          : ""}
-      </Text>
-      <Text>{gameInfo?.questions?.[progressIndex]?.question || ""}</Text>
-      {gameInfo?.questions?.[progressIndex]?.options?.map((prop, index) => {
-        return (
-          <TouchableOpacity key={index} style={styles.button}>
-            <Text style={{ color: "white" }}>{prop}</Text>
-          </TouchableOpacity>
-        );
-      })}
-    </View>
+  function handleOptionPress(question: Question, indexSelected: number): void {
+    const questionCorrect: boolean =
+      question.correctOption === question.options[indexSelected];
+    setQuestionCorrect(questionCorrect);
+    if (questionCorrect) {
+      setNumCorrect(numCorrect + 1);
+    }
+    setGameState("answer");
+  }
+
+  function handleNextQuestion(): void {
+    setProgressIndex(progressIndex + 1);
+    setGameState("question");
+  }
+
+  function handleGoToResults(): void {
+    setGameState("result");
+  }
+
+  const nextQuestionTemplate = (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => handleNextQuestion()}
+    >
+      <Text style={{ color: "white" }}>Next Question</Text>
+    </TouchableOpacity>
   );
+
+  const resultsTemplate = (
+    <TouchableOpacity style={styles.button} onPress={() => handleGoToResults()}>
+      <Text style={{ color: "white" }}>Results</Text>
+    </TouchableOpacity>
+  );
+
+  const actualGame =
+    gameState === "question" ? (
+      <View>
+        <Text
+          style={{
+            fontSize: 16,
+            paddingTop: 8,
+            paddingBottom: 8,
+          }}
+        >
+          {gameInfo?.totalQuestions
+            ? [
+                "Question",
+                progressIndex + 1,
+                "of",
+                gameInfo.totalQuestions,
+              ].join(" ")
+            : ""}
+        </Text>
+        <Text>{gameInfo?.questions?.[progressIndex]?.question || ""}</Text>
+        {gameInfo?.questions?.[progressIndex]?.options?.map((prop, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              style={styles.button}
+              onPress={() =>
+                handleOptionPress(gameInfo.questions[progressIndex], index)
+              }
+            >
+              <Text style={{ color: "white" }}>{prop}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    ) : gameState === "answer" ? (
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={require(`../assets/${
+            questionCorrect ? "correct" : "incorrect"
+          }.png`)}
+        />
+        {!questionCorrect ? (
+          <Text>
+            {"The correct answer was " +
+              gameInfo.questions[progressIndex].correctOption}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {gameInfo.totalQuestions === progressIndex + 1
+          ? resultsTemplate
+          : nextQuestionTemplate}
+      </View>
+    ) : (
+      <View>
+        <Text>Final Results</Text>
+        <Text>
+          You got {Math.round((numCorrect / gameInfo.totalQuestions) * 100)}%!
+        </Text>
+        <Button
+          title="New Game"
+          onPress={() => navigation.navigate("Game Options")}
+          color={"#384061"}
+        />
+      </View>
+    );
 
   return (
     <View style={styles.container}>
@@ -132,5 +218,10 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     padding: 16,
     margin: 16,
+  },
+  image: {
+    height: 300,
+    width: 300,
+    resizeMode: "center",
   },
 });
